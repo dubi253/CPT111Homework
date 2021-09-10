daf1ee7d34667be74f3d80d7fe985da1758e8f90
Dubi253 <45878355+dubi253@users.noreply.github.com>
2021-09-10 16:48:44 +0800

    Create SimpleMath.java

---
+++ CW1 1.1 Simple Math/SimpleMath.java
@@ -0,0 +1,11 @@
+public class SimpleMath {
+    public static void main(String[] args) {
+//        System.out.println("num1 + num2 = 13\nnum1 / num2 = 3\nreal1 + real2 = 13.0\nreal1 / real2 = 3.3333333333333335");
+        int num1 = 10, num2 = 3;
+        double real1 = 10, real2 = 3;
+        System.out.println("num1 + num2 = " + (num1 + num2));
+        System.out.println("num1 / num2 = " + (num1 / num2));
+        System.out.println("real1 + real2 = " + (real1 + real2));
+        System.out.println("real1 / real2 = " + (real1 / real2));
+    }
+}
