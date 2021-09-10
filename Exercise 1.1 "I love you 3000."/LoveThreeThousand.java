dd1928ac30fc027d264189a132b8d562871cfce1
Dubi253 <45878355+dubi253@users.noreply.github.com>
2021-09-10 16:46:16 +0800

    Create LoveThreeThousand.java

---
+++ Exercise 1.1 "I love you 3000."/LoveThreeThousand.java
@@ -0,0 +1,6 @@
+public class LoveThreeThousand {
+    public static void main(String[] args) {
+        int threeThousands = 3000;
+        System.out.println("\"I love you " + threeThousands + ".\"");
+    }
+}
